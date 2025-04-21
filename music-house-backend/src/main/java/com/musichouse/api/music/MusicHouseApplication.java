4d797705dfdd292985677118b937bccff7eda2b6
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-04-21 19:56:10 -0400

    Corregir la URL del localhost en el mensaje de inicio de MusicHouseApplication y agregar un salto de línea en el Dockerfile

--- music-house-backend/src/main/java/com/musichouse/api/music/MusicHouseApplication.java
+++ music-house-backend/src/main/java/com/musichouse/api/music/MusicHouseApplication.java
@@ -16,7 +16,7 @@ public class MusicHouseApplication {
     public static void main(String[] args) {
         SpringApplication.run(MusicHouseApplication.class, args);
         LOGGER.info("🎹 Let's hit the keys and start the musical journey with MusicHouseApplication! 🎵🌟" +
-                " http://localhost:9090 || http://llocalhost:3000");
+                " http://localhost:9090 || http://localhost:3000");
 
 
     }
