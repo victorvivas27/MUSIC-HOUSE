b707047e9658e544f744c231617e4f9158ff5b9a
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-04-22 12:12:37 -0400

    Eliminar registros de BASE_URL en theme.js y users.js para limpiar el código

--- music-house-frontend/src/api/theme.js
+++ music-house-frontend/src/api/theme.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { handleApiError } from './handleApiError';
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-console.log("🔥 BASE_URL desde env:", BASE_URL);
+
 
 
 export const getTheme = async (page=0,size=10,sort="themeName,asc") => {
