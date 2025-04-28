a901a2fbd4ef8b7279830034c47cc519154a83af
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-04-28 12:21:19 -0400

    fix(CorsGlobalConfig.java, SecurityConfig.java): refactor configuración de CORS y corregir ruta de categorías en la configuración de seguridad

--- music-house-backend/src/main/java/com/musichouse/api/music/config/CorsGlobalConfig.java
+++ music-house-backend/src/main/java/com/musichouse/api/music/config/CorsGlobalConfig.java
@@ -1,30 +1,18 @@
 package com.musichouse.api.music.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsGlobalConfig {
+public class CorsGlobalConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "https://musichouse-production.up.railway.app"
-
-
-        ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
-
