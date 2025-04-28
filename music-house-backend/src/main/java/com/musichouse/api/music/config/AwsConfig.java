270a0b1d174db0e3845fc1d5f7487a2422815cca
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-04-27 22:39:57 -0400

    fix(AwsConfig): descomentar propiedades de configuración de AWS S3 y ajustar validaciones de credenciales
    fix(application-dev.properties): cambiar niveles de logging a INFO para desarrollo
    
    fix(application-prod.properties): actualizar comentarios y ajustar configuración de logging en producción

--- music-house-backend/src/main/java/com/musichouse/api/music/config/AwsConfig.java
+++ music-house-backend/src/main/java/com/musichouse/api/music/config/AwsConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsConfig {
 
-    /*@Value("${aws.s3.access-key}")
+    @Value("${aws.s3.access-key}")
     private String accessKeyId;
 
     @Value("${aws.s3.secret-key}")
@@ -45,29 +45,5 @@ public class AwsConfig {
 
         System.out.println("AmazonS3 Client Bean Created Successfully!");
         return s3Client;
-    }*/
-
-    @Value("${aws.s3.access-key:}")
-    private String accessKeyId;
-
-    @Value("${aws.s3.secret-key:}")
-    private String accessSecretKey;
-
-    @Value("${aws.s3.region:}")
-    private String region;
-
-    @Bean
-    public AmazonS3 amazonS3() {
-        if (accessKeyId.isBlank() || accessSecretKey.isBlank() || region.isBlank()) {
-            System.out.println("AWS credentials not set. AmazonS3 client will not be created.");
-            return null;
-        }
-
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKeyId, accessSecretKey);
-
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.fromName(region))
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .build();
     }
 }
