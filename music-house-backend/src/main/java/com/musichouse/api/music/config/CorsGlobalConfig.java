1fc6af51c30234069a6416d6d1c2d53abefbc2b1
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-04-28 09:46:39 -0400

    fix(CorsGlobalConfig.java): actualizar la configuración de CORS para permitir solo el origen de producción

--- music-house-backend/src/main/java/com/musichouse/api/music/config/CorsGlobalConfig.java
+++ music-house-backend/src/main/java/com/musichouse/api/music/config/CorsGlobalConfig.java
@@ -10,7 +10,7 @@ public class CorsGlobalConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("https://musichouse-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
