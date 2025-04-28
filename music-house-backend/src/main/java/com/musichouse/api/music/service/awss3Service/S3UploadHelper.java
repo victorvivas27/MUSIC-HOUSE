87fae3fee6920e456d0e3da74f21e7871a98ebc0
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-04-27 23:30:19 -0400

    fix(S3UploadHelper): refactor para usar AwsConfig en lugar de AmazonS3 directamente y mejorar manejo de cliente S3

--- music-house-backend/src/main/java/com/musichouse/api/music/service/awss3Service/S3UploadHelper.java
+++ music-house-backend/src/main/java/com/musichouse/api/music/service/awss3Service/S3UploadHelper.java
@@ -3,6 +3,7 @@ package com.musichouse.api.music.service.awss3Service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.musichouse.api.music.config.AwsConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,7 +15,7 @@ import java.util.List;
 @Component
 public class S3UploadHelper {
 
-    private final AmazonS3 amazonS3;
+    private final AwsConfig awsConfig;
 
     @Value("${aws.s3.bucket-name}")
     private String bucketName;
@@ -22,11 +23,20 @@ public class S3UploadHelper {
     @Value("${aws.s3.region}")
     private String region;
 
-    public S3UploadHelper(AmazonS3 amazonS3) {
-        this.amazonS3 = amazonS3;
+    public S3UploadHelper(AwsConfig awsConfig) {
+        this.awsConfig = awsConfig;
+    }
+
+    private AmazonS3 getS3Client() {
+        AmazonS3 client = awsConfig.createS3Client();
+        if (client == null) {
+            throw new IllegalStateException("Amazon S3 Client not available!");
+        }
+        return client;
     }
 
     public String uploadSingleFile(MultipartFile file, String folder) {
+        AmazonS3 amazonS3 = getS3Client();
         try {
             String newFilename = folder + "/" + System.currentTimeMillis() + "_" + file.getOriginalFilename();
 
@@ -39,8 +49,7 @@ public class S3UploadHelper {
             return "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + newFilename;
 
         } catch (IOException e) {
-            throw new RuntimeException(
-                    "Error al cargar el archivo a S3", e);
+            throw new RuntimeException("Error al cargar el archivo a S3", e);
         }
     }
 
@@ -51,7 +60,6 @@ public class S3UploadHelper {
         for (MultipartFile file : files) {
             imageUrls.add(uploadSingleFile(file, folder));
         }
-
         return imageUrls;
     }
 }
