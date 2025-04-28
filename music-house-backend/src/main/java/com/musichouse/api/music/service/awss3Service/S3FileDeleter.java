7b1b296b37097ac916b74578c00b6a945fd1b3cb
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-04-27 23:34:25 -0400

    fix(S3FileDeleter): eliminar código innecesario y mejorar manejo de excepciones

--- music-house-backend/src/main/java/com/musichouse/api/music/service/awss3Service/S3FileDeleter.java
+++ music-house-backend/src/main/java/com/musichouse/api/music/service/awss3Service/S3FileDeleter.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.musichouse.api.music.config.AwsConfig;
 import com.musichouse.api.music.exception.FileNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -11,28 +12,33 @@ import org.springframework.stereotype.Component;
 import java.io.InputStream;
 
 @Component
-
 public class S3FileDeleter {
 
-    private final AmazonS3 amazonS3;
+    private final AwsConfig awsConfig;
 
     @Value("${aws.s3.bucket-name}")
     private String bucketName;
 
-    public S3FileDeleter(AmazonS3 amazonS3) {
-        this.amazonS3 = amazonS3;
+    public S3FileDeleter(AwsConfig awsConfig) {
+        this.awsConfig = awsConfig;
     }
 
+    private AmazonS3 getS3Client() {
+        AmazonS3 client = awsConfig.createS3Client();
+        if (client == null) {
+            throw new IllegalStateException("Amazon S3 Client not available!");
+        }
+        return client;
+    }
 
     public void deleteFileFromS3(String key) {
+        AmazonS3 amazonS3 = getS3Client();
         try {
             if (!doesObjectExist(key)) {
                 throw new FileNotFoundException(
                         "El archivo con clave " + key + " no existe en S3.");
             }
-
             amazonS3.deleteObject(new DeleteObjectRequest(bucketName, key));
-
         } catch (AmazonS3Exception e) {
             throw new RuntimeException(
                     "No se pudo eliminar el archivo de S3", e);
@@ -40,6 +46,7 @@ public class S3FileDeleter {
     }
 
     public boolean doesObjectExist(String key) {
+        AmazonS3 amazonS3 = getS3Client();
         try {
             return amazonS3.doesObjectExist(bucketName, key);
         } catch (RuntimeException e) {
@@ -48,6 +55,7 @@ public class S3FileDeleter {
     }
 
     public InputStream downloadFile(String key) {
+        AmazonS3 amazonS3 = getS3Client();
         if (!doesObjectExist(key)) {
             throw new FileNotFoundException(
                     "El archivo con clave " + key + " no existe en S3.");
