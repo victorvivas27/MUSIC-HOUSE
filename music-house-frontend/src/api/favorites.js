974e206a0f09177d272b1b5aeff82bfe283b2d39
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-11-07 15:25:05 -0300

    fix: Refactor favorites API functions for improved readability and consistency

--- music-house-frontend/src/api/favorites.js
+++ music-house-frontend/src/api/favorites.js
@@ -1,10 +1,13 @@
+import axios from 'axios'
+import { handleApiError } from './handleApiError'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-import axios from 'axios';
-import { handleApiError } from './handleApiError';
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-
-export const getFavoritesByUserId = async (idUser, page = 0, size = 5, sort = 'registDate,desc') => {
+export const getFavoritesByUserId = async (
+  idUser,
+  page = 0,
+  size = 5,
+  sort = 'registDate,desc'
+) => {
   try {
     const response = await axios.get(`${BASE_URL}/favorites/${idUser}`, {
       params: { page, size, sort }
@@ -15,15 +18,14 @@ export const getFavoritesByUserId = async (idUser, page = 0, size = 5, sort = 'r
   }
 }
 
-
 export const toggleFavorite = async (idUser, idInstrument) => {
   try {
     const response = await axios.post(`${BASE_URL}/favorites`, {
       idUser,
-      idInstrument,
-    });
-    return response.data;
+      idInstrument
+    })
+    return response.data
   } catch (error) {
-    handleApiError(error);
+    handleApiError(error)
   }
-};
+}
